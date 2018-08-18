60c03dad78b74b46cae6552060d01e8886682350
el-tocino <email+github@aforkinthetoad.com>
2018-08-17 22:48:08 -0500

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# adjust as necessary...
+BITRATE=16000
+
+
 NFN=$(echo $1 | sed s/.mp3//)
-ffmpeg -i $1 -acodec pcm_s16le -ac 1 -ar 16000 ${NFN}.wav
+ffmpeg -i $1 -acodec pcm_s16le -ac 1 -ar $BITRATE ${NFN}.wav
 sox  ${NFN}.wav ${NFN}_split.wav silence 1 0.1 1% 1 .66 1% : newfile : restart
 rm ${NFN}.wav
